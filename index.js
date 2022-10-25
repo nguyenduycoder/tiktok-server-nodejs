d050a2694c3eb94b699a26a48a7b01d28f998ac1
DuyNguyen <duynguyen@DuyNguyens-MacBook-Pro.local>
2022-10-25 16:24:06 +0700

    update 2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors())
 var userRouter = require('./src/routers/user.router')
 var loginRouter = require('./src/routers/login.router')
 var uploadRouter = require('./src/routers/upload.router')
-app.get('/', (req, res) => res.send('hello worl'))
+
 app.use('/api/', userRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/upload', uploadRouter)
